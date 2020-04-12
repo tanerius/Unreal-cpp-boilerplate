bf0289e4989d434af924631cd85624546302031e
Taner Selim <tanerius@gmail.com>
2020-04-12 13:20:04 +0200

    Added a custom HUD class too. GameMode is now a ready boilerplate.

---
+++ Source/CustomTemplate/CustomHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CustomHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMTEMPLATE_API ACustomHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
